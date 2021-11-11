3730599703748585115a3e24cde9eb17541ad578
DonatellaC <donatella.carta@mailbox.org>
2021-11-11 13:28:16 +0100

    feat: added background image welcome page

--- src/screens/Welcome/Welcome.js
+++ src/screens/Welcome/Welcome.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import MeditationOptions from "../../components/MeditationOptions/MeditationOptions";
-import "./Welcome.css";
+import { LandscapeWelcome } from "./Welcome.styled.js";
 
 function Welcome() {
   return (
-    <div>
+    <LandscapeWelcome>
       <Header />
       <section className="content">
         <h2 className="sub-title">What would you like to do today?</h2>
       </section>
       <MeditationOptions />
-    </div>
+    </LandscapeWelcome>
   );
 }
 
---
+++ src/screens/Welcome/Welcome.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+import landscape from "../../images/landscape.jpg";
+
+export const LandscapeWelcome = styled.div`
+  background: url(${landscape}) no-repeat center center fixed;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+`;
